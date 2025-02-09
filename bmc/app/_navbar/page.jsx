77fcd3acdd3ad5892fc20e3487ac9c8b4804a24f
ghimiresk <ghimiresushank08@gmail.com>
2025-02-09 21:38:30 +0545

    Refactor navbar component: replace Kinde authentication with Clerk integration, updating user authentication UI and removing unused code

--- bmc/app/_navbar/page.jsx
+++ bmc/app/_navbar/page.jsx
@@ -1,18 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import {
-  LoginLink,
-  LogoutLink,
-  useKindeBrowserClient,
-} from "@kinde-oss/kinde-auth-nextjs";
-import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React from "react";
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+  SignInButton,
+  SignOutButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
 
 function Header() {
   const Menu = [
@@ -21,11 +16,7 @@ function Header() {
     { id: 3, name: "Locations", path: "/locations" },
   ];
 
-  const { user } = useKindeBrowserClient();
-
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
+  const { isSignedIn } = useUser();
 
   return (
     <div className="fixed w-full backdrop-blur-sm bg-white/10 shadow-md transition-all duration-300 p-4 flex items-center justify-between rounded-b-lg z-50">
@@ -49,40 +40,18 @@ function Header() {
       </div>
 
       {/* User Authentication */}
-      {user ? (
-        <Popover>
-          <PopoverTrigger>
-            <Image
-              src={
-                user.picture ||
-                "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png"
-              }
-              alt="profile-image"
-              width={40}
-              height={40}
-              className="rounded-full border border-white"
-            />
-          </PopoverTrigger>
-          <PopoverContent className="w-44 bg-white shadow-lg rounded-lg">
-            <ul className="flex flex-col gap-2">
-              <Link
-                href="/my-booking"
-                className="hover:bg-gray-100 p-2 rounded-md"
-              >
-                My Booking
-              </Link>
-              <li className="hover:bg-gray-100 p-2 rounded-md">
-                <LogoutLink>Logout</LogoutLink>
-              </li>
-            </ul>
-          </PopoverContent>
-        </Popover>
+      {isSignedIn ? (
+        <UserButton />
       ) : (
-        <LoginLink>
+        <>
           <Button className="bg-primary text-white hover:bg-primary/80">
-            Get Started
+            {isSignedIn ? (
+              <SignOutButton />
+            ) : (
+              <SignInButton forceRedirectUrl={"/sign-in"} />
+            )}
           </Button>
-        </LoginLink>
+        </>
       )}
     </div>
   );
