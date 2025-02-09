72501f3e2f08553a9f26fb463ee9bc2e48348ee6
aarushinternational294 <166803505+aarushinternational294@users.noreply.github.com>
2025-02-09 18:42:26 +0545

    Save work before merging to main

--- bmc/app/layout.jsx
+++ bmc/app/layout.jsx
@@ -1,5 +1,16 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
+
+//clerk auth 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -21,12 +32,25 @@ export default function RootLayout({
   children,
 }) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
         {children}
       </body>
     </html>
+    </ClerkProvider>
+
   );
 }
