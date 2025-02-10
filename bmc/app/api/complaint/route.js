36506b5c56ff1b8bf0021061200cf7ad3bba1250
ghimiresk <ghimiresushank08@gmail.com>
2025-02-10 17:11:28 +0545

    Implement complaint management API with CRUD operations and integrate with Zustand store

--- bmc/app/api/complaint/route.js
+++ bmc/app/api/complaint/route.js
@@ -1,25 +1,94 @@
 import { NextResponse } from "next/server";
+import { Complaint } from "../../../models/Complaint.models";
+import { connectToDatabase } from "@/utils/db";
 
 // To get all the complaints
-export function GET() {
+export async function GET() {
   try {
-  } catch (error) {}
+    await connectToDatabase();
+    const complaints = await Complaint.find();
+    return NextResponse.json({
+      message: "Get all complaints",
+      complaints,
+      success: true,
+    });
+  } catch (error) {
+    return NextResponse.json({ message: error.message, success: false });
+  }
 }
 
 // To create a new complaint
-export function POST() {
+export async function POST(req) {
   try {
-  } catch (error) {}
-}
+    const {
+      companyName,
+      email,
+      phoneNum,
+      imageUrl,
+      complaintType,
+      complaintDescription,
+      clerkId,
+      busNumber,
+    } = await req.json();
+    await connectToDatabase();
+    const data = [
+      companyName,
+      email,
+      phoneNum,
+      imageUrl,
+      complaintType,
+      complaintDescription,
+      clerkId,
+      busNumber,
+    ];
+    if (data.some((field) => !field)) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 400 }
+      );
+    }
+    const newComplaint = new Complaint({
+      companyName,
+      email,
+      phoneNum,
+      imageUrl,
+      complaintType,
+      complaintDescription,
+      clerkId,
+      busNumber,
+    });
 
-// To update a complaint
-export function PUT() {
-  try {
-  } catch (error) {}
+    await newComplaint.save();
+  } catch (error) {
+    return NextResponse.json(
+      { message: error.message, success: false },
+      { status: 500 }
+    );
+  }
 }
 
 // To delete a complaint
-export function DELETE() {
+export async function DELETE(req) {
   try {
-  } catch (error) {}
+    await connectToDatabase();
+    const { complaintId } = await req.json();
+    if (!complaintId) {
+      return NextResponse.json(
+        {
+          message: "Post id is compulsory to delete the post",
+        },
+        { status: 400 }
+      );
+    }
+    const deletedComplaint = await Complaint.findByIdAndDelete(id);
+
+    if (!deletedComplaint) {
+      return NextResponse.json(
+        { message: "Complaint not found", success: false },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({ message: error.message, success: false });
+  }
 }
