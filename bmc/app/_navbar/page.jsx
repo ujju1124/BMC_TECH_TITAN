c8d78523161395e6148a066dafce75a386599886
aarushinternational294 <166803505+aarushinternational294@users.noreply.github.com>
2025-02-11 00:08:43 +0545

    name changed in navbar

--- bmc/app/_navbar/page.jsx
+++ bmc/app/_navbar/page.jsx
@@ -25,7 +25,7 @@ function Header() {
       <div className="flex items-center gap-10">
         <Link href="/">
           <h1 className="text-xl font-bold text-black cursor-pointer">
-            Path Finder
+            UrbanFlow
           </h1>
         </Link>
 
